a354941ac3144909825f0afb714b14f328d1f349
danielferrer <danybarco@gmail.com>
2022-11-24 07:10:40 +0100

    Initial Commit

---
+++ location/locationApp.swift
@@ -0,0 +1,17 @@
+//
+//  locationApp.swift
+//  location
+//
+//  Created by Daniel Ferrer on 24/11/22.
+//
+
+import SwiftUI
+
+@main
+struct locationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
